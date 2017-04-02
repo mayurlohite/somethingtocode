03e8c368ad817b3c1b994bdc1afb7b11f65a4151
Mayur Lohite <mayur.lohite@outlook.com>
2017-04-02 17:18:47 +0530

    Added AdminLTE theme and designed Signup and Signin Pages.

---
+++ SomethingToCode.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SomethingToCode.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
