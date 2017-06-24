6bc0b37aea045f4ad1d2007f5b49dbb055155356
Mayur Lohite <mayur.lohite@nullplex.com>
2017-06-24 12:29:26 +0530

    Added Unity Dependancy Container

---
+++ SomethingToCode.Web/App_Start/UnityConfig.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Practices.Unity;
+using Owin;
+using SomethingToCode.Data.Context;
+using SomethingToCode.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SomethingToCode.Web.App_Start
+{
+    public static class UnityConfig
+    {
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
+
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            // TODO: Register your types here
+            // container.RegisterType<IProductRepository, ProductRepository>();
+            //container.RegisterType<Models.ITest, Models.Test>();
+
+            container.RegisterType<DbContext, SCContext>(new HierarchicalLifetimeManager());
+            container.RegisterType(typeof(IRepository<>), typeof(GenericRepository<>));
+
+
+        }
+
+        public static void UseUnity(this IAppBuilder app)
+        {
+            var container = UnityConfig.GetConfiguredContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        private const string HttpContextKey = "perRequestContainer";
+
+        private readonly IUnityContainer _container;
+
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (typeof(IController).IsAssignableFrom(serviceType))
+            {
+                return ChildContainer.Resolve(serviceType);
+            }
+
+            return IsRegistered(serviceType) ? ChildContainer.Resolve(serviceType) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (IsRegistered(serviceType))
+            {
+                yield return ChildContainer.Resolve(serviceType);
+            }
+
+            foreach (var service in ChildContainer.ResolveAll(serviceType))
+            {
+                yield return service;
+            }
+        }
+
+        protected IUnityContainer ChildContainer
+        {
+            get
+            {
+                var childContainer = HttpContext.Current.Items[HttpContextKey] as IUnityContainer;
+
+                if (childContainer == null)
+                {
+                    HttpContext.Current.Items[HttpContextKey] = childContainer = _container.CreateChildContainer();
+                }
+
+                return childContainer;
+            }
+        }
+
+        public static void DisposeOfChildContainer()
+        {
+            var childContainer = HttpContext.Current.Items[HttpContextKey] as IUnityContainer;
+
+            if (childContainer != null)
+            {
+                childContainer.Dispose();
+            }
+        }
+
+        private bool IsRegistered(Type typeToCheck)
+        {
+            var isRegistered = true;
+
+            if (typeToCheck.IsInterface || typeToCheck.IsAbstract)
+            {
+                isRegistered = ChildContainer.IsRegistered(typeToCheck);
+
+                if (!isRegistered && typeToCheck.IsGenericType)
+                {
+                    var openGenericType = typeToCheck.GetGenericTypeDefinition();
+
+                    isRegistered = ChildContainer.IsRegistered(openGenericType);
+                }
+            }
+
+            return isRegistered;
+        }
+    }
+}
