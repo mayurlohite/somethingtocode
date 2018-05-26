3ec4b20eef1d214f2d50fdc37ced485568e05802
Mayur Lohite <mayur.lohite@nullplex.com>
2018-05-26 11:16:22 +0530

    Added home index page .

--- SomethingToCode.DbServices/Masters/Tags/TagService.cs
+++ SomethingToCode.DbServices/Masters/Tags/TagService.cs
@@ -8,22 +8,8 @@ using System.Threading.Tasks;
 
 namespace SomethingToCode.DbServices.Masters.Tags
 {
-    public class TagService
+    public class TagService 
     {
-        void Insert(Tag tag);
-
-        void Update(Tag tag);
-
-        void Delete(Tag tag);
-
-        Tag GetTagById(long TagID);
-
-        Tag GetTagBySlugUrl(string slugUrl, bool? isEnable = true);
-
-        IPagedList<Tag> GetAllTags(int? page, bool? isEnable = true);
-
-        IPagedList<Tag> GetTagsByUserId(int? page, long UserID, bool? isEnable = true);
-
-        IPagedList<Tag> GetTagsBySearchCriteria(int? page, long? UserID, string categoryName, bool? isEnable = true);
+        
     }
 }
